3d0f481da526d5717200c06c5e175159e4974d41
ciaranightingale <ciara.nightingale@sky.com>
2023-05-19 20:30:01 +0100

    new factory contract

--- const/constants.ts
+++ const/constants.ts
@@ -1,6 +1,6 @@
 import { Mumbai, Chain } from '@thirdweb-dev/chains'
 
-export const factoryAddress: string = '0x2FbECd71cAF8503E27D8A94bd34A67899335B25a'
+export const factoryAddress: string = '0x5a4aEa84863995AD59dECbEeFdbA138c8c064d5F'
 export const TWApiKey: string =
     '7a023c31cebbd59b3a6879fd40cc931ae97fb7e5764df9402eb3023ccca38821d455956578e40e4af2d5bf0f033462e65fd961a0b10453455eb4e074f5e7e559'
 export const activeChain: Chain = Mumbai
